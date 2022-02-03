d6fcd6b08276a0b6f792445b3ce901da241532f0
starprince7 <princeagezinweke@gmail.com>
2022-02-03 02:54:31 +0100

    Content update

--- app.js
+++ app.js
@@ -34,18 +34,13 @@ async function connectToDatabase() {
 }
 connectToDatabase()
 
-
-// Serve Static files to client only in a production Environment
-if (process.env.NODE_ENV === "production") {
-    // Make the Client files Public
-    app.use(express.static("client/react-app/build"));
+// Make the Client files Public
+app.use(express.static("client/react-app/build"));
   
-    app.get("*", (req, res) => {
-      console.log("req just came in to load up React client/react-app/build - Files ");
-      res.sendFile("./client/react-app/build/index.html", { root: __dirname });
-    });
-}
-
+app.get("*", (req, res) => {
+  console.log("req just came in to load up React client/react-app/build - Files ");
+  res.sendFile("./client/react-app/build/index.html", { root: __dirname });
+});
 
 // Middle ware To parse request body
 app.use(express.json())
